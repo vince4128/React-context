c767efa5836c38855c3ba8af6605f961569d4a31
Vincent Varlet <vincent.varlet@adevinta.com>
2019-10-21 17:37:46 +0200

    adding a color theme provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext'
+import ColorContext from '../contexts/ColorContext'
 
 class App extends Component {
-    state = { language: 'english'};
+    state = { 
+        language: 'english',
+        theme: 'primary'
+    };
 
     onLanguageChange = language => {
         this.setState({ language })
         console.log('coucou ', this.state.language)
     }
+
+    onThemeChange = e => {
+        this.setState({theme: e.target.value});
+      }
 
     render() {
         return (
@@ -18,9 +26,22 @@ class App extends Component {
                     <i className="flag gb" onClick={() => this.onLanguageChange('english')}></i>
                     <i className="flag fr" onClick={() => this.onLanguageChange('french')}></i>
                 </div>
-                <LanguageContext.Provider value={this.state.language}>
-                  <UserCreate/>
-                </LanguageContext.Provider>
+                <div>
+                    Select a theme:
+                    <select 
+                        placeholder="select a theme"
+                        value={this.state.theme}
+                        onChange={this.onThemeChange}
+                    >
+                        <option value="primary">Blue</option>
+                        <option value="red">Red</option>
+                    </select>
+                </div>
+                <ColorContext.Provider value={this.state.theme}>
+                    <LanguageContext.Provider value={this.state.language}>
+                        <UserCreate/>
+                    </LanguageContext.Provider>
+                </ColorContext.Provider>
             </div>
         )
     }
