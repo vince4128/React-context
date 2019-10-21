2f0f9f0a8c7711a2a32900220805b45a8e205f53
Vincent Varlet <vincent.varlet@adevinta.com>
2019-10-21 17:37:52 +0200

    component pulling data from multiple contexts

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
 import LanguageContext from '../contexts/LanguageContext'
+import ColorContext from '../contexts/ColorContext'
 
 class Button extends Component {
-    static contextType = LanguageContext
+    renderSubmit(value) {
+        return value === 'english' ? 'Submit' : 'Envoyer'
+    }
+
+    renderButton(color) {
+        return (
+            <button className={`ui button ${color}`}>
+                <LanguageContext.Consumer>
+                    {(value) => this.renderSubmit(value)}
+                </LanguageContext.Consumer>
+            </button>
+        )
+    }
 
     render() {
-        const text =this.context === 'english' ? 'Submit' : 'Envoyer'
-        return <button className="ui button primary">{text}</button>
+        return (
+            <ColorContext.Consumer>
+                {(color) => this.renderButton(color)}
+            </ColorContext.Consumer>
+        )
     }
 }
 
