922184ab35627353bf3c1afa04866b178a290a8a
Vincent Varlet <vincent.varlet@adevinta.com>
2019-10-21 11:26:57 +0200

    Render component into App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,24 @@
 import React, {Component} from 'react';
+import UserCreate from './UserCreate';
 
 class App extends Component {
+    state = { language: 'english'};
+
+    onLanguageChange = language => {
+        this.setState({ language })
+    }
+
     render() {
-        return <div>App</div>
+        return (
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i className="flag gb" onClick={() => this.onLanguageChange('english')}></i>
+                    <i className="flag fr" onClick={() => this.onLanguageChange('french')}></i>
+                </div>
+                <UserCreate/>
+            </div>
+        )
     }
 }
 
