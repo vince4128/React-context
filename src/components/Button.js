f1080354dcce9ef798b91cee6ab142a148dad987
Vincent Varlet <vincent.varlet@adevinta.com>
2019-10-21 14:25:57 +0200

    use Context Type to access data in Context Object

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
+import LanguageContext from '../contexts/LanguageContext'
 
 class Button extends Component {
+    static contextType = LanguageContext
+
     render() {
-        return <button className="ui button primary">Submit</button>
+        const text =this.context === 'english' ? 'Submit' : 'Envoyer'
+        return <button className="ui button primary">{text}</button>
     }
 }
 
